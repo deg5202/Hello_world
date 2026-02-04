3c812cdf26b463b8ca602d08feaf7b329c9695b5
DongHyung <deg5202@gmail.com>
2026-02-04 11:46:36 +0900

    GameManager 추가
    스크립트 추가 ( 디버그  추가)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("게임매니저 추가.");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
